09b222aad671d443b6fca7f8c8ca5cb54dbd2089
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-01-22 23:18:56 +0000

    update the folloing files   modified:   assets/css/style.css         modified:   assets/script/script.js         modified:   index.html. Previus script js doent work properly, localstorage missing after refresh page on gitHub. New style for buttuns

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
-    // Display current day at the top of the calendar
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  let currentHour = new Date().getHours();
 
-//color-code each timeblock based on past, present and future 
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
- let currentHour = moment().hours();
-  $(".description").each(function(){
-   let bloclHour = parseInt($(this).attr("id").split("am"));
-    if (bloclHour < currentHour){
-      $(this).addClass("past");
-    }else if (bloclHour === currentHour){
-      $(this).addClass("present");
-    }else{
-      $(this).addClass("future");
+  // Create timeblocks for each hour from 9am to 5pm
+  for (let i = 9; i <= 17; i++) {
+    let hour = i <= 12 ? `${i}am` : `${i - 12}pm`;
+    let timeblock = $("<div>").addClass("row");
+
+    let time = $("<div>").addClass("col-sm-1 hour").text(hour);
+    timeblock.append(time);
+
+    let event = $("<input>").addClass("col-sm-10 description");
+    timeblock.append(event);
+
+    let saveBtn = $("<button>").addClass("col-sm-1 saveBtn fa-solid fa-floppy-disk");
+    timeblock.append(saveBtn);
+    
+    $("#timeblocks").append(timeblock);
+
+    // Check if timeblock is past, present, or future
+    if (i < currentHour) {
+      event.addClass("past");
+    } else if (i === currentHour) {
+      event.addClass("present");
+    } else {
+      event.addClass("future");
     }
-  });
 
-//save event when the button is clicked 
-  $(".saveBtn").on("click" , function(){
-    let event = $(this).prev().val();
-    let time = $(this).attr("id").split("-")[0];
-    localStorage.setItem(time, event);
-  });
+    // Save event in local storage when save button is clicked
+    saveBtn.click(function() {
+      localStorage.setItem(hour, event.val());
+    });
 
-  // Retrieve and display saved events when page is refreshed 
-  //Repeat the above line of code for each hour from 9am to 5pm
-    $("#9am").val(localStorage.getItem("9am"));
-    $("#10am").val(localStorage.getItem("10am"));
-    $("#11am").val(localStorage.getItem("11am"));
-    $("#12am").val(localStorage.getItem("12am"));
-    $("#13pm").val(localStorage.getItem("13pm"));
-    $("#14pm").val(localStorage.getItem("14pm"));
-    $("#15pm").val(localStorage.getItem("15pm"));
-    $("#16pm").val(localStorage.getItem("16pm"));
-    $("#17pm").val(localStorage.getItem("17pm"));
-  });
+    // Retrieve and display saved events when page is refreshed
+    event.val(localStorage.getItem(hour));
+  }
+});
 
