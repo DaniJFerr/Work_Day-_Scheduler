d5cd2200e2938b25b7ca048245184516ddc54d50
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-01-22 15:04:37 +0000

    define js code that add class to the timeblock section based on present hour time. The classes color is defined on css file

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,5 +2,23 @@ $(document).ready(function() {
     // Display current day at the top of the calendar
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+//color-code each timeblock based on past, present and future 
+
+  var currentHour = moment().hours();
+  $(".description").each(function(){
+    var bloclHour = parseInt($(this).attr("id").split("am"));
+    if (bloclHour < currentHour){
+      $(this).addClass("past");
+    }else if (bloclHour === currentHour){
+      $(this).addClass("present");
+    }else{
+      $(this).addClass("future");
+    }
+    console.log(this);
+
+  });
+
+  
+
   });
 
